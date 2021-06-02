160fc64cd1af863e8520cd1bfa53f74c6618f5af
Mariana10-04 <marianagondasilva@gmail.com>
2021-06-01 22:56:51 -0300

    tentativa 2

--- prova0106/src/main/java/com/prova/prova016/controller/ClienteController.java
+++ prova0106/src/main/java/com/prova/prova016/controller/ClienteController.java
@@ -1,5 +1,50 @@
-package com.prova.prova016.controller;
+package com.jpa.basic.controller;
 
-public class ClienteController {
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.jpa.basic.dto.ClientDTO;
+import com.jpa.basic.entity.Client;
+import com.jpa.basic.service.ClientService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+    
+    @GetMapping("/")
+    public List<Client> getClients(){
+        return clientService.getClients();
+    }
+
+    @GetMapping("/clientsdto")
+    public List<ClientDTO> getClientsDto(){
+        List<Client> result = clientService.getClients();
+        return result.stream().map(client -> new ClientDTO(client)).collect(Collectors.toList());
+    }
+    
+    @PostMapping("/")
+    public Client newClient(@RequestBody Client client){
+        clientService.saveClient(client);
+        return client;
+    }
+    
+    @PutMapping("/{id}")
+    public Client updatedClient(@PathVariable Long id, @RequestBody Client client){
+        return clientService.updateClient(id, client);
+    }
+    
+    @DeleteMapping("/{id}")
+    public String deleteCLient(@PathVariable Long id){
+        return clientService.deleteClient(id);
+    }
+    
+    
     
 }
+
